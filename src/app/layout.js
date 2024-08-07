b57a1cc922c5ee4e5d2a063ba7eec5b3dedf78ac
Grameco <113364758+Grameco@users.noreply.github.com>
2024-08-08 00:31:56 +0530

    Favicon, middle text

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="icon" href="/logo.png" sizes="any" />
       <body className={inter.className}>
         <Toaster />
         {children}
