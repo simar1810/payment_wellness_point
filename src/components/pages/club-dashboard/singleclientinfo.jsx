f42b3b97c27d39ccc6735d26224d0a8a8e627c17
dnyaneshwar411 <dnyaneshwarpk20@gmail.com>
2024-10-22 21:52:47 +0530

    fixes: rollnumber and active issue in special-mode

--- src/components/pages/club-dashboard/singleclientinfo.jsx
+++ src/components/pages/club-dashboard/singleclientinfo.jsx
@@ -1,5 +1,6 @@
 import { EyeIcon } from "@/components/svgs";
 import apiInstance from "@/helpers/api";
+import { Modal } from "@mui/material";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import toast from "react-hot-toast";
@@ -64,6 +65,16 @@ export default function SingleClientinfo({ index, detail, onMutateDep }) {
                 In-Active
               </button>
             )
+          ) : clubSystem === 3 ? (
+            detail.specialMode?.pointsHistory?.length > 0 ? (
+              <button className="h-[30px] px-4 min-w-[98px] text-white bg-[#03632C] rounded">
+                Active
+              </button>
+            ) : (
+              <button className="w-full h-[30px] max-w-[98px] text-white bg-[#EA4335] rounded">
+                In-Active
+              </button>
+            )
           ) : (
             <button className="w-full h-[30px] max-w-[98px] text-white bg-[#EA4335] rounded">
               In-Active
@@ -78,7 +89,10 @@ export default function SingleClientinfo({ index, detail, onMutateDep }) {
         </p>
       </div>
       <div className="w-full h-[1px] bg-[#EEEEEE]"></div>
-      {editOpen && <EditRollNoComponent onMutateDep={onMutateDep} detail={detail} onDiscardEditing={() => setEditOpen(false)} />}
+      {editOpen && <EditRollNoComponent
+        onMutateDep={onMutateDep}
+        detail={detail}
+        onDiscardEditing={() => setEditOpen(false)} />}
     </div>
   );
 }
@@ -88,7 +102,7 @@ function EditRollNoComponent({ detail, onDiscardEditing, onMutateDep }) {
     try {
       e.preventDefault()
       const data = {
-        newRollNumber: `${detail.rollno.slice(0, 3)}${e.currentTarget.newRollNumber.value}`,
+        newRollNumber: e.currentTarget.newRollNumber.value,
         clientId: detail._id
       }
       const response = await apiInstance.editClientRollNumber(data);
@@ -104,15 +118,17 @@ function EditRollNoComponent({ detail, onDiscardEditing, onMutateDep }) {
   }
 
 
-  return <div className="max-w-[600px] mx-auto mt-10 bg-[#FFF] shadow-2xl p-8 rounded-md relative border-2">
-    <button onClick={onDiscardEditing} className="absolute right-4 top-4">
-      <FaXmark c="black" className="w-10 h-10" />
-    </button>
-    <form onSubmit={handleEditRollNo}>
-      <FormControl title={`Roll Number - starts from - ${detail.rollno.slice(0, 3)}`} name="newRollNumber" defaultValue={detail.rollno.slice(3)} />
-      <button type='submit' className="text-w hite bg-green-400 block mt-4 mx-auto px-4 py-2 rounded-md">Update</button>
-    </form>
-  </div>
+  return <Modal open={true} className="flex items-center justify-center">
+    <div className="w-full max-w-[450px] bg-white p-4 relative rounded-md">
+      <button onClick={onDiscardEditing} className="absolute right-4 top-4 rounded-md z-[1000]">
+        <FaXmark c="black" className="w-6 h-6" />
+      </button>
+      <form onSubmit={handleEditRollNo}>
+        <FormControl title="Change rollno" name="newRollNumber" defaultValue={detail.rollno} />
+        <button type='submit' className="text-white bg-green-800 block mt-4 mx-auto px-4 py-2 rounded-md">Update</button>
+      </form>
+    </div>
+  </Modal>
 }
 
 function FormControl({ title, name, ...props }) {
