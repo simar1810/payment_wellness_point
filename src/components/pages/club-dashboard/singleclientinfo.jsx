0438f8e3980dbf667616242ca375823d4d73425d
dnyaneshwar411 <dnyaneshwarpk20@gmail.com>
2025-01-24 10:41:03 +0530

    feat: minor changes

--- src/components/pages/club-dashboard/singleclientinfo.jsx
+++ src/components/pages/club-dashboard/singleclientinfo.jsx
@@ -1,5 +1,6 @@
 import { EyeIcon } from "@/components/svgs";
 import apiInstance from "@/helpers/api";
+import { userUpdateDetailConfig } from "@/utils/configData";
 import { Modal } from "@mui/material";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
@@ -17,15 +18,38 @@ export default function SingleClientinfo({ index, detail, onMutateDep }) {
     <div className="w-full px-3">
       <div className="w-full text-sm py-1 text-[#292D32] font-medium flex items-center justify-around gap-1">
         <p className="w-[5%] text-center">{index + 1}</p>
-        <p className="w-[12%] max-w-40 text-center">{detail.name}</p>
+
+        <EditCustomerField
+          value={detail.name}
+          _id={detail._id}
+          fieldName="name"
+          keyname="name"
+          userName={detail.name}
+          onMutateDep={onMutateDep}
+        />
         <p className="w-[10%] flex justify-center items-center gap-1">
           {detail.rollno}
           <button onClick={() => setEditOpen(true)}>
             <FaEdit c="green" />
           </button>
         </p>
-        <p className="w-[12%] text-center">{detail.sponseredByName}</p>
-        <p className="w-[12%] text-center">{detail.mobileNumber}</p>
+        <EditCustomerField
+          value={detail.sponseredByName}
+          _id={detail._id}
+          fieldName="Sponsor"
+          keyname="sponseredByName"
+          userName={detail.name}
+          onMutateDep={onMutateDep}
+        />
+        <EditCustomerField
+          value={detail.mobileNumber}
+          _id={detail._id}
+          fieldName="Mobile Number"
+          keyname="mobileNumber"
+          userName={detail.name}
+          onMutateDep={onMutateDep}
+        />
+        {/* <p className="w-[12%] text-center">{detail.mobileNumber}</p> */}
         {clubSystem === 2 && <>
           <p className="w-[10%] text-center">{parseInt(detail?.activePoints).toFixed(2)}</p>
           <p className="w-[12%] max-w-40 text-center">{Math.round(parseInt(detail?.activePoints) / parseInt(detail?.coachVolumePoint))}</p>
@@ -80,12 +104,19 @@ export default function SingleClientinfo({ index, detail, onMutateDep }) {
             </button>
           )}
         </div>
-        <p
-          onClick={() => router.push(`/club-clients/${clientId}`)}
-          className="w-[5%] flex justify-center cursor-pointer"
-        >
-          <EyeIcon h={20} w={25} c={"black"} />
-        </p>
+        <div className="w-[5%] flex justify-center gap-2 cursor-pointer">
+          <button onClick={() => router.push(`/club-clients/${clientId}`)}>
+            <EyeIcon
+              h={20}
+              w={25}
+              c={"black"}
+            />
+          </button>
+          <MultipleFields
+            detail={detail}
+            onMutateDep={onMutateDep}
+          />
+        </div>
       </div>
       <div className="w-full h-[1px] bg-[#EEEEEE]"></div>
       {editOpen && <EditRollNoComponent
@@ -130,9 +161,139 @@ function EditRollNoComponent({ detail, onDiscardEditing, onMutateDep }) {
   </Modal>
 }
 
-function FormControl({ title, name, ...props }) {
+function FormControl({
+  title,
+  name,
+  className,
+  ...props
+}) {
   return <div className="my-4">
-    <label className="text-w hite">{title}</label>
-    <input type={props.type || "text"} name={name} className="w-full block focus:outline-none mt-2 px-4 py-2 rounded-md border-2" {...props} />
+    <label className={`text-w hite ${className}`}>{title}</label>
+    <input
+      type={props.type || "text"}
+      name={name}
+      className="input w-full block focus:outline-none mt-2 px-4 py-2 rounded-md border-2"
+      {...props}
+    />
   </div>
+}
+
+function EditCustomerField({
+  value,
+  fieldName = "name",
+  keyname,
+  _id,
+  userName,
+  onMutateDep
+}) {
+  const [modalOpened, setModalOpened] = useState(false)
+
+  async function updateDetails(e) {
+    try {
+      e.preventDefault();
+      const data = {
+        [keyname]: e.currentTarget[keyname].value
+      }
+      const response = await apiInstance.updateClient(data, _id)
+      if (response?.data?.status) {
+        toast.success(response?.data?.message || "Updated details successfully");
+        onMutateDep()
+      } else {
+        toast.error(response?.data?.message || "Please try again later!");
+      }
+    } catch (error) {
+      toast.error(error?.response?.data?.message || error.message || "Please try again later!");
+    }
+  }
+
+  return <>
+    <button
+      onClick={() => setModalOpened(true)}
+      className="w-[12%] max-w-40 text-center flex items-center justify-center gap-1 cursor-pointer hover:font-semibold [&_.edit]:hover:opacity-100"
+    >
+      <span>{value}</span>
+      <FaEdit className="edit w-[12px] h-[12px] cursor-pointer opacity-60" />
+    </button>
+    {modalOpened && <Modal
+      open={true}
+      onClose={() => setModalOpened(false)}
+      className="flex items-center justify-center"
+    >
+      <div className="max-w-[450px] w-full bg-white p-4 rounded-md relative">
+        <FaXmark
+          onClick={() => setModalOpened(false)}
+          className="w-[20px] h-[20px] absolute top-3 right-4 cursor-pointer"
+        />
+        <h2 className="text-[20px] font-bold leading-tight mb-4">Edit {fieldName} of {userName}</h2>
+        <form onSubmit={updateDetails}>
+          <FormControl
+            title={fieldName}
+            className="capitalize"
+            name={keyname}
+            defaultValue={value}
+          />
+          <button type="submit" className="bg-green-800 text-white text-[14px] font-semibold leading-[1] px-4 py-2 rounded-md">Update</button>
+        </form>
+      </div>
+    </Modal>}
+  </>
+}
+
+function MultipleFields({
+  detail,
+  onMutateDep
+}) {
+  const [modalOpened, setModalOpened] = useState(false)
+
+  async function updateDetails(e) {
+    try {
+      e.preventDefault();
+      const data = {}
+      for (const config of userUpdateDetailConfig) {
+        data[config.name] = e.currentTarget[config.name].value
+      }
+      const response = await apiInstance.updateClient(data, detail._id)
+      if (response?.data?.status) {
+        toast.success(response?.data?.message || "Updated details successfully");
+        onMutateDep()
+      } else {
+        toast.error(response?.data?.message || "Please try again later!");
+      }
+    } catch (error) {
+      toast.error(error?.response?.data?.message || error.message || "Please try again later!");
+    }
+  }
+
+  return <>
+    <button
+      onClick={() => setModalOpened(true)}
+      className="text-center flex items-center justify-center gap-1 cursor-pointer"
+    >
+      <FaEdit className="w-[16px] h-[16px] cursor-pointer" />
+    </button>
+    {modalOpened && <Modal
+      open={true}
+      onClose={() => setModalOpened(false)}
+      className="flex items-center justify-center"
+    >
+      <div className="max-w-[450px] w-full max-h-[70vh] bg-white p-4 pb-0 overflow-y-auto rounded-md relative">
+        <FaXmark
+          onClick={() => setModalOpened(false)}
+          className="w-[20px] h-[20px] absolute top-3 right-4 cursor-pointer"
+        />
+        <h2 className="text-[20px] font-bold leading-tight mb-4">Edit details of {detail.name}</h2>
+        <form onSubmit={updateDetails}>
+          {userUpdateDetailConfig.map(config => <FormControl
+            key={config.id}
+            {...config}
+            defaultValue={detail[config.name] || ""}
+          />
+          )}
+          <div className="pt-4 pb-2 bg-white sticky bottom-0">
+            <button type="submit" className="bg-green-800 text-white text-[14px] font-semibold leading-[1] px-4 py-2 rounded-md">Update</button>
+          </div>
+        </form>
+      </div>
+    </Modal>}
+  </>
 }
---
+++ src/utils/configData.js
@@ -0,0 +1,33 @@
+export const userUpdateDetailConfig = [
+  {
+    id: 1,
+    name: "name",
+    title: "Name of Customer"
+  },
+  {
+    id: 2,
+    name: "email",
+    title: "Email of Customer",
+    type: "email"
+  },
+  {
+    id: 3,
+    name: "rollno",
+    title: "Roll No of Customer",
+  },
+  {
+    id: 4,
+    name: "mobileNumber",
+    title: "Mobile Number No of Customer",
+  },
+  {
+    id: 5,
+    name: "city",
+    title: "City No of Customer",
+  },
+  {
+    id: 6,
+    name: "sponseredByName",
+    title: "Sponsor No of Customer",
+  },
+]
